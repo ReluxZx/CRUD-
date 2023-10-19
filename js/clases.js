d3267f404de5f4739723c110ed91c8f366f086c5
AlexisHz2023 <alexisdurango721@gmail.com>
2023-10-19 07:23:14 -0500

    images

--- js/clases.js
+++ js/clases.js
@@ -2,8 +2,8 @@ export class Gift {
     constructor(id, gift, tipo, tiempo, precio, imagen) {
         this.id = id;
         this.gift = gift;
-        this.tiempo = tiempo;
         this.tipo = tipo;
+        this.tiempo = tiempo;
         this.precio = precio;
         this.imagen = imagen;
     }
